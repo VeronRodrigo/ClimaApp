735982025095971ee8cb641f2b091333b2f5aa0d
Rodrigo Veron <rodrigo.veron1@outlook.com>
2024-02-08 17:09:41 -0300

    Cambios finales

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', () => {
+
             function getWeatherByLocation(latitude, longitude) {
                 const apiKey = '8cd289075f84687c78da13bf871cbae4';
                 const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&lang=es&appid=${apiKey}`;
@@ -6,14 +7,18 @@ window.addEventListener('load', () => {
                 fetchWeather(apiUrl);
             }
             
-            //Buscador
             
             function getWeatherByCity(city) {
                 const apiKey = '457a2f1677b4f3cbce0df2e12aa31d1b'; 
                 const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=es&appid=${apiKey}`;
             
                 fetchWeather(apiUrl);
+
             }
+
+            loadSearchHistory();
+
+
             async function fetchWeather(apiUrl) {
                 try {
                     const response = await fetch(apiUrl);
@@ -39,23 +44,58 @@ window.addEventListener('load', () => {
                     console.error('Error fetching weather data:', error);
                 }
             }
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition((position) => {
-                    const {latitude, longitude} = position.coords;
-                    getWeatherByLocation (latitude, longitude);
-                });
-            } else {
-                showError ('La ubicacion no esta disponible');
-                return;
-            }
 
-            document.getElementById('buscar-btn').addEventListener('click', () =>{
-                const city = document.getElementById('ciudad-input').value.trim();
-                if (city !== ''){
-                    getWeatherByCity(city);
-                } else {
-                    showError ('Ingrese el nombre de una ciudad.');
-                    return;
-                }
+
+        function loadSearchHistory() {
+            const searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+            const historyList = document.getElementById('search-history');
+            
+            historyList.innerHTML = ''; 
+            
+            searchHistory.forEach(info => {
+                const listItem = document.createElement('li');
+                listItem.textContent = info.city + " " + info.temperature + " °C";
+                historyList.appendChild(listItem);
             });
-        });
+    }
+
+        function saveToSearchHistory(info) {
+            let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+            if (!searchHistory.find(object => object.city === info.city)) {
+                searchHistory.push(info);
+                localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+                loadSearchHistory();
+            }
+        }
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                const {latitude, longitude} = position.coords;
+                getWeatherByLocation (latitude, longitude);
+            });
+        } else {
+            showError ('La ubicacion no esta disponible');
+            return;
+        }
+
+    document.getElementById('buscar-btn').addEventListener('click', () => {
+        searchWeather();
+    });
+
+    document.getElementById('ciudad-input').addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') {
+            searchWeather();
+        }
+    });
+
+    function searchWeather() {
+        const city = document.getElementById('ciudad-input').value.trim();
+        const temperature = document.getElementById('temperature').innerHTML.trim();
+        if (city !== '' && temperature !== "") {
+            getWeatherByCity(city);
+            saveToSearchHistory({city, temperature});
+        } else {
+            alert('Por favor, ingrese el nombre de una ciudad.');
+        }
+    }
+});
